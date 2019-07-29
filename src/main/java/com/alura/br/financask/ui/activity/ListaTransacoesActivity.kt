404c3b76090582828baa9ce408b2c16eb970eec5
Andre <skiter.andre@gmail.com>
2019-07-29 20:11:12 -0300

    adicionado datapickesr

--- src/main/java/com/alura/br/financask/ui/activity/ListaTransacoesActivity.kt
+++ src/main/java/com/alura/br/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,13 +1,22 @@
 package com.alura.br.financask.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import com.alura.br.financask.R
+import com.alura.br.financask.extension.formataParaBrasileiro
 import com.alura.br.financask.model.Tipo
 import com.alura.br.financask.model.Transacao
 import com.alura.br.financask.ui.ResumoView
 import com.alura.br.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -21,6 +30,47 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         configuraResumo(transacoes)
         configuraLista(transacoes)
+
+        lista_transacoes_adiciona_receita.setOnClickListener {
+
+            val view: View = window.decorView
+            val viewCriada = LayoutInflater.from(this)
+                    .inflate(R.layout.form_transacao, view as ViewGroup,
+                            false)
+
+
+            val dia = 29
+            val mes = 6
+            val ano = 2019
+
+            val hoje = Calendar.getInstance()
+
+            viewCriada.form_transacao_data.setText(hoje.formataParaBrasileiro())
+
+            viewCriada.form_transacao_data.setOnClickListener {
+                DatePickerDialog(this,
+                        DatePickerDialog.OnDateSetListener { datePicker, ano, mes, dia ->
+                            val dataSelecionada = Calendar.getInstance()
+                            dataSelecionada.set(ano, mes, dia)
+                            viewCriada.form_transacao_data.setText(dataSelecionada.formataParaBrasileiro())
+
+                        }
+                , ano , mes, dia).show()
+
+            }
+
+
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_receita)
+                    .setView(viewCriada)
+                    .show()
+        }
+
+
+        lista_transacoes_adiciona_despesa.setOnClickListener {
+            Toast.makeText(this, "Adiciona despesa", Toast.LENGTH_LONG).show()
+        }
+
     }
 
     private fun configuraResumo(transacoes: List<Transacao>){
